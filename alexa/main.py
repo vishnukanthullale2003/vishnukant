0e36adf26043404891141b5e5f126bb7c4bc4fe3
vishnukanthullale2003 <vvishnu8722@gmail.com>
2024-07-19 08:43:55 +0530

    Add files via upload

---
+++ alexa/main.py
@@ -0,0 +1,68 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
+                print(command)
+    except:
+        pass
+    return command
+
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('Current time is ' + time)
+    elif 'who the heck is' in command:
+        person = command.replace('who the heck is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'date' in command:
+        talk('sorry, I have a headache')
+    elif 'are you single' in command:
+        talk('I am in a relationship with wifi')
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    elif 'who is the principal of appa college' in command:
+        talk('the principal is vinita mam')
+    elif 'who is the head of computer science department' in command:
+        talk('the head is veeresh biradar')
+    elif 'who created you' in command:
+        talk('vishnukant patil is created me')
+    elif 'where is appa college' in command:
+        talk('appa college is located in bidar')
+    else:
+        talk('Please say the command again.')
+
+
+while True:
+    run_alexa()
